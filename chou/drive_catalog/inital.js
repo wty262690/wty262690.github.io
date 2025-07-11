9a751308be6049aa53f45acdbc3d1149564a9948
unknown <wty262690@gmail.com>
2025-07-11 19:56:19 +0100

    change margin auto

--- chou/drive_catalog/inital.js
+++ chou/drive_catalog/inital.js
@@ -4,7 +4,7 @@ $(window).on('load', function () {
 
 var path = "../images/price/artwork";
 
-for (var i = 2; i<=113 ; i++){
-    $(".content").append('<div class = "img" ><img src="'+path+i+'.jpg" alt=""  loading="lazy" /></div>');
+for (var i = 2; i<=113 ; i+=2){
+    $(".content").append('<div class = "img" ><img src="'+path+i+'.jpg" alt=""  loading="lazy" /><img src="'+path+(i+1)+'.jpg" alt=""  loading="lazy" /></div>');
 }
-$(".content").append('<div><img id = "back_cover"  src="../images/back.jpg" alt=""  loading="lazy" /></div>');
+$(".content").append('<div class= "img"><img id = "back_cover"  src="../images/back.jpg" alt=""  loading="lazy" /></div>');
