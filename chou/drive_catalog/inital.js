f6dc37087cc26636e798886e11224784c096a442
unknown <wty262690@gmail.com>
2025-07-11 19:08:46 +0100

    chou change drive

---
+++ chou/drive_catalog/inital.js
@@ -0,0 +1,10 @@
+$(window).on('load', function () {
+  $('#loading').fadeOut(500,);
+});
+
+var path = "../images/price/artwork";
+
+for (var i = 2; i<=113 ; i++){
+    $(".content").append('<div class = "img" ><img src="'+path+i+'.jpg" alt=""  loading="lazy" /></div>');
+}
+$(".content").append('<div><img id = "back_cover"  src="../images/back.jpg" alt=""  loading="lazy" /></div>');
