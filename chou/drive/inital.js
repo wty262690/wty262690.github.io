545c1536d8d359be08708c41abdc226f3eb7af28
unknown <wty262690@gmail.com>
2025-07-13 23:36:01 +0100

    add connect information

--- chou/drive/inital.js
+++ chou/drive/inital.js
@@ -8,4 +8,5 @@ var path = "../images/artwork/作品集";
 for (var i = 2; i<=113 ; i+=2){
     $(".content").append('<div class = "img" ><img src="'+path+i+'.jpg" alt=""  loading="lazy" /><img src="'+path+(i+1)+'.jpg" alt=""  loading="lazy" /></div>');
 }
+$(".content").append('<div class = "img" ><img src="../images/作品集117.jpg" alt=""  loading="lazy" /><img src="../images/作品集118.jpg" alt=""  loading="lazy" /></div>');
 $(".content").append('<div class= "img"><img id = "back_cover"  src="../images/back.jpg" alt=""  loading="lazy" /></div>');
--- chou/inital.js
+++ chou/inital.js
@@ -4,6 +4,13 @@ $(window).on('load', function () {
 
 var path = "./images/artwork/作品集";
 
-for (var i = 2; i<=114 ; i++){
+$(".flipbook").append('<div><img src="./images/作品集3.jpg" alt=""  loading="lazy" /></div>');
+$(".flipbook").append('<div><img src="./images/作品集4.jpg" alt=""  loading="lazy" /></div>');
+
+for (var i = 2; i<=113 ; i++){
     $(".flipbook").append('<div><img src="'+path+i+'.jpg" alt=""  loading="lazy" /></div>');
 }
+
+$(".flipbook").append('<div><img src="./images/作品集117.jpg" alt=""  loading="lazy" /></div>');
+$(".flipbook").append('<div><img src="./images/作品集118.jpg" alt=""  loading="lazy" /></div>');
+$(".flipbook").append('<div><img src="'+path+'114.jpg" alt=""  loading="lazy" /></div>');
